4401ae3c66cdf09e08dc910022654d60411c29b0
Raquel Adrados <radrados00@gmail.com>
2023-05-10 19:28:21 +0200

    Add array function

---
+++ src/fizzBuzz.ts
@@ -0,0 +1,20 @@
+const arrayNumbers = (numberInitial: number, numberFinal: number) => {
+  let arrayContentsNumbers = [];
+  for (let index = numberInitial; index <= numberFinal; index++) {
+    arrayContentsNumbers.push(index);
+    if (index % 3 === 0) {
+      arrayContentsNumbers.pop();
+      arrayContentsNumbers.push("Fizz");
+    }
+    if (index % 5 === 0) {
+      arrayContentsNumbers.pop();
+      arrayContentsNumbers.push("Buzz");
+    }
+    if (index % 3 === 0 && index % 5 === 0) {
+      arrayContentsNumbers.pop();
+      arrayContentsNumbers.push("FizzBuzz");
+    }
+  }
+  return arrayContentsNumbers;
+};
+console.log(arrayNumbers(2, 5));
